c16d97aa12a1ca097ce52ab6a0de1c09c8c4d602
Kareem Otafy <kareemetefy@gmail.com>
2022-07-05 20:40:59 +0200

    app init

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOjxiCGtAp8Gj2hi_GcjFc1nV4o-0c6jA",
+  authDomain: "videoapp-129d6.firebaseapp.com",
+  projectId: "videoapp-129d6",
+  storageBucket: "videoapp-129d6.appspot.com",
+  messagingSenderId: "898903776141",
+  appId: "1:898903776141:web:24294bc0afe6f98c783bc4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
